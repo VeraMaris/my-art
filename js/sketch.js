75dbd93e8fb439c0be162a8da3e0bf4b3dedf99d
VeraMaris <71328838+VeraMaris@users.noreply.github.com>
2020-09-15 15:05:37 +0200

    my first commit

---
+++ js/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+
+createCanvas (200,200);
+background (100);
+print("dit is een setup");
+
+rectMode(CENTER)
+ellipse(100,100,200,200);
+fill(255,0,0);
+rect(100,100,20,20);
+
+fill(0);
+textAlign(CENTER,CENTER)
+text('dit is het midden', 100, 100);
+
+}
+
+function draw() {
+
+
+}
